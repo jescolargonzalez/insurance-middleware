a8491c8cc179092203ec8b110492c51bbae7a611
jescolargonzalez <jesusescolargonzalez@gmail.com>
2023-02-16 12:04:25 +0100

    Implementada la generacion de controladores del middle y de clientes rest.
    Implementado filtro de autenticación que llama a /validate del core

---
+++ src/main/java/com/tfm/aseguradora/backend/middle/config/RestClientConfiguration.java
@@ -0,0 +1,29 @@
+package com.tfm.aseguradora.backend.middle.config;
+
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.context.annotation.*;
+import org.springframework.web.client.*;
+
+
+@Configuration
+public class RestClientConfiguration {
+
+    @Value("${rest.client.core.users.host}")
+    private String baseUsersHost;
+
+    @Bean
+    @Primary
+    public com.tfm.aseguradora.backend.middle.users.ApiClient securityClient() {
+        var userApiClient = new com.tfm.aseguradora.backend.middle.users.ApiClient();
+        userApiClient.setBasePath(baseUsersHost);
+        return userApiClient;
+    }
+
+    @Bean
+    @Primary
+    public RestTemplate restTemplate() {
+        var restTemplate = new RestTemplate();
+        return restTemplate;
+    }
+
+}
